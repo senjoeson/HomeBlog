3731c87bdc8098be373343d175354842bb283a27
speedhero <speedhero@126.com>
2018-06-13 11:08:32 +0800

    SpingBoot  事务管理

--- blog-core/src/test/java/com/rick/blogcore/BlogCoreApplicationTests.java
+++ blog-core/src/test/java/com/rick/blogcore/BlogCoreApplicationTests.java
@@ -1,16 +1,29 @@
 package com.rick.blogcore;
 
+import com.rick.blogcore.persistence.beans.BizArticleLook;
+import com.rick.blogcore.persistence.mapper.BizArticleLookMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@EnableTransactionManagement //如果mybatis中service实现类中加入事务注解，需要此处添加该注解
+@MapperScan("com.rick.blogcore.persistence.mapper")  //扫描的是mapper.xml中namespace指向值的包位置
 public class BlogCoreApplicationTests {
 
+	@Autowired
+	private BizArticleLookMapper mapper;
 	@Test
 	public void contextLoads() {
+	List<BizArticleLook> BizArticleLooks=mapper.selectAll();
+	System.out.println(BizArticleLooks);
 	}
 
 }
